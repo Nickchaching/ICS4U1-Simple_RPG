3d2a52c26b5d7e10d88e42a343fd7ae0b83b0dd9
Nickchaching <nicholaslching@gmail.com>
2023-09-24 22:05:20 -0400

    Scene 0, 1, 2a, 2d Updated

--- Simple_RPG.java
+++ Simple_RPG.java
@@ -17,6 +17,8 @@ public class Simple_RPG{
 	private static Color clrRed = new Color(255, 0, 0);
 	private static Color clrGreen = new Color(0, 255, 0);
 	private static Color clrBlue = new Color(0, 0, 255);
+	private static Color clrShield = new Color(65, 105, 225);
+	private static Color clrDamage = new Color(255, 87, 51);
 	
 	//Global Fonts
 	private static Font fnt24 = con.loadFont("BebasNeue.ttf", 24);
@@ -40,7 +42,8 @@ public class Simple_RPG{
 		//2 - GAME
 			//2.1 - Map
 			//2.2 - Fighting
-			//2.3 - Regen and Buff
+			//2.3 - Died DT Drowning
+			//2.4 - Died DT Enemy
 		//3 - HELP
 		//4 - END GAME
 		
@@ -48,27 +51,48 @@ public class Simple_RPG{
 		int intMapWidth = 20;
 		int intMapHeight = 20;
 		
+		//Defining Character Start Position
+		int intCharXStart = 1;
+		int intCharYStart = 1;
+		
+		//Defining Input Delay
+		int intDelay = 50;
+		
 		//Main Function Variables
-		String strMap[][];							//0 - INITILIZATION
+		String strMap[][] = new String[intMapHeight][intMapWidth];		//0 - INITILIZATION
 		String strFileLine;
 		String strFileSplit[];
 		int intCountRows;
 		int intCountColumns;
-		int intMouseX = 0;								//1 - MENU
+		int intMouseX = 0;												//1 - MENU
 		int intMouseY = 0;
 		int intMouseClicked;
 		boolean blnClicked;
-		char chrMovement;							//2.1 - MAP
+		char chrMovement;												//2.1 - MAP
+		int intCharX = intCharXStart;
+		int intCharY = intCharXStart;
+		int intCharXLast = intCharX;
+		int intCharYLast = intCharY;
+		int intDamage;
+		int intDefence;
+		int intHealth;
+		int intMaxDamage;
+		int intFullHealth;
+		int intFullDefence;
+		int intEnemyDamage;
+		int intEnemyDefence;
+		int intEnemyHealth;
+		boolean blnTrans;
+		char chrPlayAgain;												//2.4 - DIED DT DROWNING		
 
 		
 		//Main Scene Loop
 		while(dblScene >= 0){
 			
-			//Scene 0 - Initilization
+			//Scene 0 - INITILIZATION
 			while(dblScene == 0){
 
 				//Importing Map File and Creating Array
-				strMap = new String[intMapHeight][intMapWidth];
 				TextInputFile txtMap = new TextInputFile("map.csv");
 				
 				//Loading Array
@@ -79,6 +103,10 @@ public class Simple_RPG{
 						strMap[intCountRows][intCountColumns] = strFileSplit[intCountColumns];
 					}
 				}
+				
+				//Resetting Variables
+				intCharX = intCharXStart;
+				intCharY = intCharYStart;
 				
 				//Closing File
 				txtMap.close();
@@ -91,7 +119,7 @@ public class Simple_RPG{
 				
 			}
 			
-			//Scene 1 - Menu
+			//Scene 1 - MENU
 			while(dblScene == 1){
 				
 				//Scene Graphics
@@ -105,6 +133,7 @@ public class Simple_RPG{
 				//Conditions
 				if(intMouseX > 850 && intMouseX < 1100 && intMouseY > 275 && intMouseY < 375 && intMouseClicked == 1){
 					dblScene = 2.1;
+					blnTrans = false;
 				}
 				else if(intMouseX > 850 && intMouseX < 1100 && intMouseY > 425 && intMouseY < 525 && intMouseClicked == 1){
 					dblScene = 3;
@@ -113,14 +142,92 @@ public class Simple_RPG{
 					dblScene = 4;
 				}
 				
+				Scene1Out(dblScene);
 			}
 			
 			//Scene 2.1 - MAP
 			while(dblScene == 2.1){
 				
+				//Scene Animation In
+				//if (blnTrans = false){
+					//Scene2aIn(strMap, intCharX, intCharY);
+					//blnTrans = true;
+				//}
+				
 				//Scene Input
 				chrMovement = con.currentChar();
+				
+				//Character Movement
+				if(chrMovement == 'w'){
+					intCharY = intCharY - 1;
+				}
+				else if(chrMovement == 'a'){
+					intCharX = intCharX - 1;
+				}
+				else if(chrMovement == 's'){
+					intCharY = intCharY + 1; 
+				}
+				else if(chrMovement == 'd'){
+					intCharX = intCharX + 1;
+				}
+				
+				//Character Movement Bounding
+				if(intCharX < 1){
+					intCharX = 1;
+				}
+				else if(intCharX > 20){
+					intCharX = 20;
+				}
+				else if(intCharY < 1){
+					intCharY = 1;
+				}
+				else if(intCharY > 20){
+					intCharY = 20;
+				}
+				
+				//Water Death
+				if(strMap[intCharY - 1][intCharX - 1].equals("w")){
+					dblScene = 2.3;
+				}
+				
+				//TO PROGRAM
+				//ACCURATE STATS HUD DISPLAY
+				//BUILDING INTERACTION
+				//BUFF CARRY INTERACTION
+				//FIGHTING INTERACTION
+				//When interacted, change the array value to " ", will result in a grass tile
+				
+				//MaxDamage represents the maximum amount of damage attainable through all sources of powerups
+				//FullHealth represents the current 100% state of health
+				//FullShield represents the current 100% state of shield
+				
+				
+				//Scene Graphics
+				Scene2a(strMap, intCharX, intCharY, intCharXLast, intCharYLast);
+				
+				intCharXLast = intCharX;
+				intCharYLast = intCharY;
+				
+				con.sleep(intDelay);
+				
+			}
 			
+			//Scene 2.3 - WATER DEATH
+			while(dblScene == 2.3){
+				
+				//Scene Graphics
+				Scene2c();
+				
+				//Scene Input
+				chrPlayAgain = con.getChar();
+				
+				if(chrPlayAgain == 'p'){
+					dblScene = 0;
+				}
+				else{
+					dblScene = 4;
+				}
+				
 			}
 		}
 	}
@@ -140,6 +247,10 @@ public class Simple_RPG{
 		int intSize = 1200;
 		int intVelo = 0;
 		
+		//Drawing Black Screen
+		con.setDrawColor(clrBlack);
+		con.fillRect(0, 0, 1150, 800);
+		
 		//Preparing Elements
 		con.setDrawColor(clrWhite);
 		con.setDrawFont(fnt50);
@@ -148,7 +259,7 @@ public class Simple_RPG{
 		intCAlign = CAlign(fnt50, "Loading...");
 		con.drawString("Loading...", 575 - (intCAlign[0]/2), 375 - (intCAlign[1]/2)); 
 		con.repaint();
-		con.sleep(2000);
+		con.sleep(500);
 		
 		//Preparing Elements
 		con.setDrawColor(clrBlack);
@@ -199,7 +310,7 @@ public class Simple_RPG{
 		}
 	}
 	
-	//Scene 1 - Menu
+	//Scene 1 - MENU CONTENT
 	public static void Scene1(int intMouseX, int intMouseY){	
 		//Scene Images
 		BufferedImage imgMenu = con.loadImage("Graphics/Menu Screen.jpg");
@@ -250,18 +361,249 @@ public class Simple_RPG{
 		con.repaint();
 	}
 	
-	//Scene 2 - Select Map
-	
-	//Scene 3.1 - Map
-	public static void Scene3a(){
-		BufferedImage imgTree = con.loadImage("tree.png");
-		BufferedImage imgGrass = con.loadImage("grass.png");
-		BufferedImage imgWater = con.loadImage("water.png");
+	//Scene 1 (OUT) - MENU CONTENT
+	public static void Scene1Out(double dblScene){
 		
-		con.drawImage(imgTree,0,0);
-		con.drawImage(imgGrass,0,40);
-		con.drawImage(imgWater,0,80);
+		//Scene Variables
+		int intCount;
+		int intPosX = 1150;
+		int intVelo = 0;
+		
+		//Preparing Elements (Based on Input)
+		if(dblScene == 2.1){
+			con.setDrawColor(clrGreen);
+		}
+		else if(dblScene == 3){
+			con.setDrawColor(clrBlue);
+		}
+		else if(dblScene == 4){
+			con.setDrawColor(clrRed);
+		}
+		
+		//Transition Out if Input Selected
+		if(dblScene != 1){
+			for(intCount = 0; intCount < 45; intCount++){
+				con.fillRect(intPosX, 0, 1150, 800);
+				intVelo = intVelo + 3;
+				intPosX = intPosX - intVelo;
+				con.repaint();
+				con.sleep(17);
+			}
+		}
+	}
+	
+	//Scene 2.1 - MAP
+	public static void Scene2a(String strMap[][], int intCharX, int intCharY, int intCharXLast, int intCharYLast){
+		
+		//Scene Images
+		BufferedImage imgTree = con.loadImage("Graphics/tree.png");
+		BufferedImage imgGrass = con.loadImage("Graphics/grass.png");
+		BufferedImage imgWater = con.loadImage("Graphics/water.png");
+		BufferedImage imgBuild = con.loadImage("Graphics/build.png");
+		BufferedImage imgHero = con.loadImage("Graphics/hero.png");
+		BufferedImage imgHP100 = con.loadImage("Graphics/health100.png");
+		BufferedImage imgDamage = con.loadImage("Graphics/damage.png");
+		BufferedImage imgShield = con.loadImage("Graphics/shield.png");
+		
+		//Font Alignment Variable (Coordinate Based)
+		int intCAlign[] = new int[2];
+		
+		//Scene Variables
+		int intCount;
+		int intCount2;
+		int intCount3;
+		int intVelo = 0;
+		int intMove = 0;
+		
+		//Drawing HUD Box
+		con.setDrawColor(clrBlack);
+		con.fillRect(800, 0, 350, 800);
+		
+		//Drawing HUD Stats Fill
+		con.setDrawColor(clrDamage);
+		con.fillRect(825, 575, 200, 50);
+		con.setDrawColor(clrShield);
+		con.fillRect(825, 650, 200, 50);
+		con.setDrawColor(clrRed);
+		con.fillRect(825, 725, 200, 50);
+		
+		//Preparing Elements
+		con.setDrawColor(clrWhite);
+		con.setDrawFont(fnt30);
+		
+		//Drawing HUD Outline and Elements
+		con.drawRect(825, 575, 300, 50);
+		con.drawImage(imgDamage, 840, 585);
+		con.drawRect(825, 650, 300, 50);
+		con.drawImage(imgShield, 840, 660);
+		con.drawRect(825, 725, 300, 50);
+		con.drawImage(imgHP100, 840, 735);
+		
+		//Drawing HUD Text
+		intCAlign = CAlign(fnt30, "30/30");
+		con.drawString("30/30", 975 - (intCAlign[0]/2), 592 - (intCAlign[1]/2));
+		intCAlign = CAlign(fnt30, "30/30");
+		con.drawString("30/30", 975 - (intCAlign[0]/2), 667 - (intCAlign[1]/2));
+		intCAlign = CAlign(fnt30, "30/30");
+		con.drawString("30/30", 975 - (intCAlign[0]/2), 742 - (intCAlign[1]/2));
+		
+		//Hero Movement Animation
+		if(intCharX > intCharXLast){
+			//Animate Right
+			for(intCount3 = 0; intCount3 < 5; intCount3++){
+				intVelo = intVelo + 2;
+				intMove = intMove + intVelo;
+				
+				//Re-Drawing Map Background
+				for(intCount = 0; intCount < 20; intCount++){
+					for(intCount2 = 0; intCount2 < 20; intCount2++){
+						if(strMap[intCount][intCount2].equals("w")){
+							con.drawImage(imgWater, (intCount2 * 40), (intCount * 40));
+						}
+						else if(strMap[intCount][intCount2].equals("t")){
+							con.drawImage(imgTree, (intCount2 * 40), (intCount * 40));
+						}
+						else if(strMap[intCount][intCount2].equals("b")){
+							con.drawImage(imgBuild, (intCount2 * 40), (intCount * 40));
+						}
+						else{
+							con.drawImage(imgGrass, (intCount2 * 40), (intCount * 40));
+						}
+					}
+				}
+				
+				//Drawing Hero
+				con.drawImage(imgHero, intMove + (intCharXLast * 40) - 40, (intCharYLast * 40) - 40);
+				con.repaint();
+				con.sleep(17);
+			}
+		}
+		else if(intCharX < intCharXLast){
+			//Animate Left
+			for(intCount3 = 0; intCount3 < 5; intCount3++){
+				intVelo = intVelo + 2;
+				intMove = intMove - intVelo;
+				
+				//Re-Drawing Map Background
+				for(intCount = 0; intCount < 20; intCount++){
+					for(intCount2 = 0; intCount2 < 20; intCount2++){
+						if(strMap[intCount][intCount2].equals("w")){
+							con.drawImage(imgWater, (intCount2 * 40), (intCount * 40));
+						}
+						else if(strMap[intCount][intCount2].equals("t")){
+							con.drawImage(imgTree, (intCount2 * 40), (intCount * 40));
+						}
+						else if(strMap[intCount][intCount2].equals("b")){
+							con.drawImage(imgBuild, (intCount2 * 40), (intCount * 40));
+						}
+						else{
+							con.drawImage(imgGrass, (intCount2 * 40), (intCount * 40));
+						}
+					}
+				}
+				
+				//Drawing Hero
+				con.drawImage(imgHero, intMove + (intCharXLast * 40) - 40, (intCharYLast * 40) - 40);
+				con.repaint();
+				con.sleep(17);
+			}
+		}
+		else if(intCharY > intCharYLast){
+			//Animate Down
+			for(intCount3 = 0; intCount3 < 5; intCount3++){
+				intVelo = intVelo + 2;
+				intMove = intMove + intVelo;
+				
+				//Re-Drawing Map Background
+				for(intCount = 0; intCount < 20; intCount++){
+					for(intCount2 = 0; intCount2 < 20; intCount2++){
+						if(strMap[intCount][intCount2].equals("w")){
+							con.drawImage(imgWater, (intCount2 * 40), (intCount * 40));
+						}
+						else if(strMap[intCount][intCount2].equals("t")){
+							con.drawImage(imgTree, (intCount2 * 40), (intCount * 40));
+						}
+						else if(strMap[intCount][intCount2].equals("b")){
+							con.drawImage(imgBuild, (intCount2 * 40), (intCount * 40));
+						}
+						else{
+							con.drawImage(imgGrass, (intCount2 * 40), (intCount * 40));
+						}
+					}
+				}
+				
+				//Drawing Hero
+				con.drawImage(imgHero, (intCharXLast * 40) - 40, intMove + (intCharYLast * 40) - 40);
+				con.repaint();
+				con.sleep(17);
+			}
+		}
+		else if(intCharY < intCharYLast){
+			//Animate Up
+			for(intCount3 = 0; intCount3 < 5; intCount3++){
+				intVelo = intVelo + 2;
+				intMove = intMove - intVelo;
+				
+				//Re-Drawing Map Background
+				for(intCount = 0; intCount < 20; intCount++){
+					for(intCount2 = 0; intCount2 < 20; intCount2++){
+						if(strMap[intCount][intCount2].equals("w")){
+							con.drawImage(imgWater, (intCount2 * 40), (intCount * 40));
+						}
+						else if(strMap[intCount][intCount2].equals("t")){
+							con.drawImage(imgTree, (intCount2 * 40), (intCount * 40));
+						}
+						else if(strMap[intCount][intCount2].equals("b")){
+							con.drawImage(imgBuild, (intCount2 * 40), (intCount * 40));
+						}
+						else{
+							con.drawImage(imgGrass, (intCount2 * 40), (intCount * 40));
+						}
+					}
+				}
+				
+				//Drawing Hero
+				con.drawImage(imgHero, (intCharXLast * 40) - 40, intMove + (intCharYLast * 40) - 40);
+				con.repaint();
+				con.sleep(17);
+			}
+		}
+		
+		//DRAWING MAP BASE
+		//Reading Rows
+		for(intCount = 0; intCount < 20; intCount++){
+			//Reading Columns
+			for(intCount2 = 0; intCount2 < 20; intCount2++){
+				if(strMap[intCount][intCount2].equals("w")){
+					con.drawImage(imgWater, (intCount2 * 40), (intCount * 40));
+				}
+				else if(strMap[intCount][intCount2].equals("t")){
+					con.drawImage(imgTree, (intCount2 * 40), (intCount * 40));
+				}
+				else if(strMap[intCount][intCount2].equals("b")){
+					con.drawImage(imgBuild, (intCount2 * 40), (intCount * 40));
+				}
+				else{
+					con.drawImage(imgGrass, (intCount2 * 40), (intCount * 40));
+				}
+			}
+		}
+		
+		//Drawing Hero's Final Position
+		con.drawImage(imgHero, (intCharX * 40) - 40, (intCharY * 40) - 40);
 		con.repaint();
+	}
+	
+	//Scene 2.4 - DIED DT DROWNING
+	public static void Scene2c(){
+		
+		//Scene Images
+		BufferedImage imgScene2dBG = con.loadImage("Graphics/Drowning Game Over.png");
+		
+		//Drawing Image
+		con.drawImage(imgScene2dBG, 0, 0);
+		con.repaint();
+		
 	}
 	
 	//Text Display Width Calculator
